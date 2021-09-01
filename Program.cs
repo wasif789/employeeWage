26ca6786481ca0980eaf0968bb743daf93ae8ebe
wasif farid <wsffarid1@gmail.com>
2021-09-01 15:44:27 +0530

    monthly wage added

--- Program.cs
+++ Program.cs
@@ -4,30 +4,35 @@ namespace empWage
 {
     class Program
     {
-
+        public const int empWage_perhour = 20;
+        public const int part_time = 1;
+        public const int full_time = 2;
+        public const int num_working_days = 20;
         static void Main(string[] args)
         {
-            int empWage_perhour = 20;
-            int part_time = 1;
-            int full_time = 2;
             int empHrs;
-            int empWage;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            int empWage, totalEmployeeWage = 0;
+            for (int day = 0; day < num_working_days; day++)
             {
-                case part_time:
-                    empHrs= 4;
-                    break;
-                case full_time:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case part_time:
+                        empHrs = 4;
+                        break;
+                    case full_time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empWage_perhour * empHrs;
+                totalEmployeeWage = totalEmployeeWage + empWage;
+                Console.WriteLine("employee wage:" + empWage);
             }
-            empWage = empWage_perhour * empHrs;
-            Console.WriteLine("employee wage:" + empWage);
+            Console.WriteLine("total employee wage:" + totalEmployeeWage);
         }
     }
 }
