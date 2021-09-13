fbcc61d0fab96207866dc21419af18711a6f850c
wasif farid <wsffarid1@gmail.com>
2021-09-13 22:54:19 +0530

    refactor with methods

--- Program.cs
+++ Program.cs
@@ -8,21 +8,21 @@ namespace empWage
         public const int FULL_TIME = 1;
         public const int ABSENT = 0;
         public const int PART_TIME = 2;
-        public const int WAGE_PER_HOUR = 20;
-        public const int WORKING_DAYS_PER_MONTH = 20;
-        public const int WORKING_HOURS_PER_MONTH = 100;
+        //public const int WAGE_PER_HOUR = 20;
+        //public const int WORKING_DAYS_PER_MONTH = 20;
+        //public const int WORKING_HOURS_PER_MONTH = 100;
 
 
-        private static void CalculateEmpWage()
+        private static void CalculateEmpWage(string company, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours)
         {
             int workingDays = 0;
             int wagesForMonth = 0;
             int workingHours = 0;
             int totalWorkingHours = 0;
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+
             Random rand = new Random(); // intializing random class
             //for loop for calculating for 20 days
-            while (totalWorkingHours <= WORKING_HOURS_PER_MONTH && workingDays < WORKING_DAYS_PER_MONTH)
+            while (totalWorkingHours <= maxWorkingHours && workingDays < workingDaysPerMonth)
             {
                 int empInput = rand.Next(0, 3);//Generating random b/w 0 and 3
                 switch (empInput)
@@ -45,14 +45,16 @@ namespace empWage
 
             }
 
-            wagesForMonth = WAGE_PER_HOUR * totalWorkingHours;//formula for wages
-            Console.WriteLine("Employee wage per month is " + wagesForMonth);
+            wagesForMonth = wagePerHour * totalWorkingHours;//formula for wages
+            Console.WriteLine($"Total EmpWage For company {company} is {wagesForMonth}");
 
         }
 
         static void Main(string[] args)
         {
-            CalculateEmpWage();
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            CalculateEmpWage("Tvs", 50, 20, 100);
+            CalculateEmpWage("Zoho", 40, 20, 200);
 
         }
     }
